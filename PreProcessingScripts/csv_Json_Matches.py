c08dd51b623e03ff7f1e31c79da7eee9950699d9
Mohan1544 <54016279+Mohan1544@users.noreply.github.com>
2020-03-14 13:51:40 -0600

    csv to Json convert for matches

---
+++ PreProcessingScripts/csv_Json_Matches.py
@@ -0,0 +1,24 @@
+import csv
+import json
+import datetime
+import re
+csvfile='D:\CUB\Spring20\BigDataArchitecture\Project\matches.csv'
+jsonfile='D:\CUB\Spring20\BigDataArchitecture\Project\matches.json'
+
+lines1=[]
+out1=[]
+
+
+with open(csvfile) as csvfile1:
+    reader_in=csv.DictReader(csvfile1)
+    rows=[]
+    for row in reader_in:
+        row['id']=int(row['id'])
+        row['season']=int(row['season'])
+        row['date']=re.sub(r'(\d+)-(\d+)-(\d+)', r'\2/\3/\1', row['date'])
+        row['dl_applied']=int(row['dl_applied'])
+        row['win_by_runs']=int(row['win_by_runs'])
+        row['win_by_wickets']=int(row['win_by_wickets'])
+        rows.append(row)
+with open(jsonfile,'w') as jsonfile1:
+    jsonfile1.write(json.dumps(rows,indent=4))
