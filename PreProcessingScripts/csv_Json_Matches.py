61423e94a8b8f8dc48010659eda33e78f6aa83d3
Mohan1544 <54016279+Mohan1544@users.noreply.github.com>
2020-03-25 18:37:54 -0600

    Update csv_Json_Matches.py

--- PreProcessingScripts/csv_Json_Matches.py
+++ PreProcessingScripts/csv_Json_Matches.py
@@ -13,12 +13,13 @@ with open(csvfile) as csvfile1:
     reader_in=csv.DictReader(csvfile1)
     rows=[]
     for row in reader_in:
-        row['id']=int(row['id'])
+        # row['id']=int(row['id'])
         row['season']=int(row['season'])
         row['date']=re.sub(r'(\d+)-(\d+)-(\d+)', r'\2/\3/\1', row['date'])
         row['dl_applied']=int(row['dl_applied'])
         row['win_by_runs']=int(row['win_by_runs'])
         row['win_by_wickets']=int(row['win_by_wickets'])
+        # print(row['date'])
         rows.append(row)
 with open(jsonfile,'w') as jsonfile1:
     jsonfile1.write(json.dumps(rows,indent=4))
