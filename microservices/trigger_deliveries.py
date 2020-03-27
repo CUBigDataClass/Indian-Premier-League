251fc57577023ee9a78c67769d76d0ceb2737567
AbhiniveshP <pabhinivesh@gmail.com>
2020-03-27 11:24:55 -0600

    trigger function for deliveries updated for each records
    Updated for each record which was for a group of records earlier.

--- microservices/trigger_deliveries.py
+++ microservices/trigger_deliveries.py
@@ -8,32 +8,29 @@ es_config = (json.loads(response['Body'].read()))
 
 
 def insert_into_es(each_doc, _id):
-    insert_request = requests.post(url="{}/{}/_doc/{}".format(es_config['es_url'], es_config['es_deliveries_index'], _id),
-                                       data=json.dumps(each_doc),
-                                       headers=es_config['es_request_headers']).json()
+    insert_request = requests.post(
+        url="{}/{}/_doc/{}".format(es_config['es_url'], es_config['es_deliveries_index'], _id),
+        data=json.dumps(each_doc),
+        headers=es_config['es_request_headers']).json()
     print(insert_request)
 
 
 def lambda_handler(event, context):
     # TODO implement
-    action = event['Records'][0]['eventName']
-    
-    if (action == 'INSERT' or action == 'MODIFY'):
 
-        currentEvent = event['Records'][0]['dynamodb']['NewImage']
-        for key, valueDict in currentEvent.items():
-            for dataType, finalValue in valueDict.items():
-                if dataType == 'N':
-                    currentEvent[key] = float(finalValue)
-                elif dataType == 'S':
-                    currentEvent[key] = str(finalValue)
-    
-        print(currentEvent)
-        insert_into_es(currentEvent, currentEvent['uid'])
-        
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    for each_ in event['Records']:
+        action = each_['eventName']
+
+        if (action == 'INSERT' or action == 'MODIFY'):
+
+            currentEvent = each_['dynamodb']['NewImage']
+            for key, valueDict in currentEvent.items():
+                for dataType, finalValue in valueDict.items():
+                    if dataType == 'N':
+                        currentEvent[key] = float(finalValue)
+                    elif dataType == 'S':
+                        currentEvent[key] = str(finalValue)
+
+            insert_into_es(currentEvent, currentEvent['uid'])
+
     # response = requests.post('http://34.227.172.158:3718/', json=json.dumps(currentEvent))
