6a3359126ff1d71492bca72953f6f9d9a2037ce1
AbhiniveshP <pabhinivesh@gmail.com>
2020-03-27 11:28:28 -0600

    Updated code to upload data to DynamoDB
    Added the functionality of timer for 60 seconds after pushing 240 rows of data to DynamoDB so that it is in sync when triggered to Elastic Search.

--- PreProcessingScripts/native_script_deliveries.py
+++ PreProcessingScripts/native_script_deliveries.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+from time import sleep
 
 dynamo_db = boto3.resource('dynamodb',
                            aws_access_key_id='ASIAZDXTUI3TWYNUWBC5',
@@ -15,8 +16,9 @@ with open('../deliveries.json') as json_file:
 count = 0
 
 for key in data:
-    if (count % 100 == 0):
+    if (count % 240 == 0):
         print(count, ' items done')
+        sleep(60)
     # if (count > 149800):
     table.put_item(Item= data[key])
     count += 1
