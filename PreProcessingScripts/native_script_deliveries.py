c8276bfae00137d79a038d447a2049bdefa54ceb
AbhiniveshP <pabhinivesh@gmail.com>
2020-04-06 12:08:27 -0600

    removed AWS access keys' values from the code

--- PreProcessingScripts/native_script_deliveries.py
+++ PreProcessingScripts/native_script_deliveries.py
@@ -3,9 +3,9 @@ import json
 from time import sleep
 
 dynamo_db = boto3.resource('dynamodb',
-                           aws_access_key_id='ASIAZDXTUI3TWYNUWBC5',
-                           aws_secret_access_key='/lXPIMTiu39F9bWKigM2Gm0yelAO2+8HBpvq3LGy',
-                           aws_session_token= 'FwoGZXIvYXdzEJP//////////wEaDCArr02mH3ISsSIqRiLEARLAhLtm37FtC0XhXMS3AzwxbL81t/udWnJGFbuF4Q6Ml5HNX8lNXS44EFajYO+UHEc4oEmJdpZHBHQxY3HZDC46p4gClTDn2o+uo9lVtW57vFwTiXQ5jp1dULeDO0YXpWZzxTiwf08a9/my/VoQtPEbVH5MGsD5GxxFiL6j5MR0+qc/hdDKi4QnRXbYozH0VmsH4lQKdtv024N0x6iVaSnphN8r5gK+32m9tvw4mKHBIPuQPDVGVK27sZf8cDUDcov0HloohKvu8wUyLf4Cl3k3g6KjOVHu81Hdl6JZNAL+dqompu0HNwztNUrbXqzATH/cVO7UvImSOA==',
+                           aws_access_key_id='...',
+                           aws_secret_access_key='...',
+                           aws_session_token= '...',
                            region_name= 'us-east-1')
 
 table = dynamo_db.Table('deliveries')
