c085ce51ac2336faedfa2458b46dbfdbaf6f8b1b
AbhiniveshP <pabhinivesh@gmail.com>
2020-04-18 19:19:09 -0600

    updated match stats microservice
    Micro service updated such that all 3 DBs updates fall into Elastic Search at the same time.

--- microservices/update_match_stats.py
+++ microservices/update_match_stats.py
@@ -17,8 +17,7 @@ def insert_into_es(each_doc, index, _id):
                                        headers=es_config['es_request_headers']).json()
         print(insert_request, 400)
 
-    except:
-        print(each_doc, index, _id)
+    except Exception as es:
         exit(0)
 
 
@@ -42,7 +41,9 @@ def update_player_info(player_id_map, pub_json, deliveries_info_response, player
 
             #   Retrieve info of both bowler and batsman
             player_info = get_response(es_config['es_players_index'], player_id)
-            match_id = match_info['match_id']
+            player_info = player_info['_source']
+
+            match_id = int(match_info['match_id'])
 
             #   Check whether matches dict is in _source
             if ('matches' not in player_info):
@@ -160,8 +161,8 @@ def update_player_info(player_id_map, pub_json, deliveries_info_response, player
 
             #   -------------------------------------------------------------------------------------------------------    #
             #   Initialize total extras and total wickets for the entire match
-            if 'total_extras' not in player_info:
-                player_info['total_extras'] = 0
+            if 'total_runs_given' not in player_info:
+                player_info['total_runs_given'] = 0
 
             if 'total_wickets' not in player_info:
                 player_info['total_wickets'] = 0
@@ -175,8 +176,8 @@ def update_player_info(player_id_map, pub_json, deliveries_info_response, player
             if 'balls_bowled' not in player_info:
                 player_info['balls_bowled'] = 0
 
-            if 'total_extras' not in player_match_info:
-                player_match_info['total_extras'] = 0
+            if 'total_runs_given' not in player_match_info:
+                player_match_info['total_runs_given'] = 0
             if 'total_wickets' not in player_match_info:
                 player_match_info['total_wickets'] = 0
             if 'balls_bowled' not in player_match_info:
@@ -185,9 +186,9 @@ def update_player_info(player_id_map, pub_json, deliveries_info_response, player
             if (player_type == 'bowler'):
 
                 #   Update total extras and total wickets for the entire match
-                if pub_json['extra_runs'] != 0:
-                    player_info['total_extras'] += pub_json['extra_runs']
-                    player_match_info['total_extras'] += pub_json['extra_runs']
+                if pub_json['total_runs'] != 0:
+                    player_info['total_runs_given'] += pub_json['total_runs']
+                    player_match_info['total_runs_given'] += pub_json['total_runs']
                 if pub_json['dismissal_kind'] != 'Not Dismissed':
                     player_info['total_wickets'] += 1
                     player_match_info['total_wickets'] += 1
@@ -218,12 +219,6 @@ def update_match_info(pub_json, match_info_response):
     if match_info_response['found']:
         match_info = match_info_response['_source']
 
-        # Initialize batsman and bowler dictionaries to setup a player's combined profile
-        if pub_json['batsman'] not in match_info:
-            match_info[pub_json['batsman']] = {}
-        if pub_json['bowler'] not in match_info:
-            match_info[pub_json['bowler']] = {}
-
         #   -------------------------------------------------------------------------------------------------------    #
         #   Initialize total runs (innings 1 and 2 separately), in parallel with balls faced if the player is a batsman
         if 'total_runs_inn1' not in match_info:
@@ -232,10 +227,6 @@ def update_match_info(pub_json, match_info_response):
             match_info['total_runs_inn2'] = 0
         if 'total_runs' not in match_info:
             match_info['total_runs'] = 0
-        if 'total_runs' not in match_info[pub_json['batsman']]:
-            match_info[pub_json['batsman']]['total_runs'] = 0
-        if 'balls_faced' not in match_info[pub_json['batsman']]:
-            match_info[pub_json['batsman']]['balls_faced'] = 0
 
         #   Update total runs (innings 1 and 2 separately), in parallel with balls faced if the player is a batsman
         if pub_json['inning'] == 1:
@@ -243,8 +234,6 @@ def update_match_info(pub_json, match_info_response):
         if pub_json['inning'] == 2:
             match_info['total_runs_inn2'] += pub_json['total_runs']
         match_info['total_runs'] += pub_json['total_runs']
-        match_info[pub_json['batsman']]['total_runs'] += pub_json['total_runs']
-        match_info[pub_json['batsman']]['balls_faced'] += 1
         #   -------------------------------------------------------------------------------------------------------    #
 
         #   -------------------------------------------------------------------------------------------------------    #
@@ -264,44 +253,27 @@ def update_match_info(pub_json, match_info_response):
         if 'total_6s' not in match_info:
             match_info['total_6s'] = 0
 
-        #   Initialize total 0s, 1s, 2s, 3s, 4s, 5s and 6s of the batsman for the entire match to zero
-        if 'total_0s' not in match_info[pub_json['batsman']]:
-            match_info[pub_json['batsman']]['total_0s'] = 0
-        if 'total_1s' not in match_info[pub_json['batsman']]:
-            match_info[pub_json['batsman']]['total_1s'] = 0
-        if 'total_2s' not in match_info[pub_json['batsman']]:
-            match_info[pub_json['batsman']]['total_2s'] = 0
-        if 'total_3s' not in match_info[pub_json['batsman']]:
-            match_info[pub_json['batsman']]['total_3s'] = 0
-        if 'total_4s' not in match_info[pub_json['batsman']]:
-            match_info[pub_json['batsman']]['total_4s'] = 0
-        if 'total_5s' not in match_info[pub_json['batsman']]:
-            match_info[pub_json['batsman']]['total_5s'] = 0
-        if 'total_6s' not in match_info[pub_json['batsman']]:
-            match_info[pub_json['batsman']]['total_6s'] = 0
-
         #   Update total 0s, 1s, 2s, 3s, 4s, 5s and 6s of the match along with the corresponding batsman accordingly
         if pub_json['total_runs'] == 0:
             match_info['total_0s'] += 1
-            match_info[pub_json['batsman']]['total_0s'] += 1
+
         if pub_json['total_runs'] == 1:
             match_info['total_1s'] += 1
-            match_info[pub_json['batsman']]['total_1s'] += 1
+
         if pub_json['total_runs'] == 2:
             match_info['total_2s'] += 1
-            match_info[pub_json['batsman']]['total_2s'] += 1
+
         if pub_json['total_runs'] == 3:
             match_info['total_3s'] += 1
-            match_info[pub_json['batsman']]['total_3s'] += 1
+
         if pub_json['total_runs'] == 4:
             match_info['total_4s'] += 1
-            match_info[pub_json['batsman']]['total_4s'] += 1
+
         if pub_json['total_runs'] == 5:
             match_info['total_5s'] += 1
-            match_info[pub_json['batsman']]['total_5s'] += 1
+
         if pub_json['total_runs'] >= 6:
             match_info['total_6s'] += 1
-            match_info[pub_json['batsman']]['total_6s'] += 1
         #   -------------------------------------------------------------------------------------------------------    #
 
         #   -------------------------------------------------------------------------------------------------------    #
@@ -373,21 +345,12 @@ def update_match_info(pub_json, match_info_response):
         #   -------------------------------------------------------------------------------------------------------    #
 
         #   -------------------------------------------------------------------------------------------------------    #
-        #   Initialize total extras and total wickets for the entire match
-        if 'total_extras' not in match_info:
-            match_info['total_extras'] = 0
-            match_info[pub_json['bowler']]['total_extras'] = 0
-        if 'total_wickets' not in match_info:
-            match_info['total_wickets'] = 0
-            match_info[pub_json['bowler']]['total_wickets'] = 0
-
         #   Update total extras and total wickets for the entire match
         if pub_json['extra_runs'] != 0:
             match_info['total_extras'] += pub_json['extra_runs']
-            match_info[pub_json['bowler']]['total_extras'] += pub_json['extra_runs']
+
         if pub_json['dismissal_kind'] != 'Not Dismissed':
             match_info['total_wickets'] += 1
-            match_info[pub_json['bowler']]['total_wickets'] += 1
         #   -------------------------------------------------------------------------------------------------------    #
 
         #   -------------------------------------------------------------------------------------------------------    #
@@ -416,23 +379,6 @@ def update_match_info(pub_json, match_info_response):
             match_info['total_extras_inn2'] += pub_json['extra_runs']
         if pub_json['dismissal_kind'] != 'Not Dismissed' and pub_json['inning'] == 2:
             match_info['total_wickets_inn2'] += 1
-        #   -------------------------------------------------------------------------------------------------------    #
-
-        #   -------------------------------------------------------------------------------------------------------    #
-        #   Initialize and update balls bowled of a particular player if balls bowled
-        if 'balls_bowled' not in match_info[pub_json['bowler']]:
-            match_info[pub_json['bowler']]['balls_bowled'] = 0
-
-        match_info[pub_json['bowler']]['balls_bowled'] += 1
-        #   -------------------------------------------------------------------------------------------------------    #
-
-        #   -------------------------------------------------------------------------------------------------------    #
-        #   Initialize or update number of wickets for a player if wicket taken
-        if pub_json['dismissal_kind'] != 'Not Dismissed':
-            if 'wickets' in match_info[pub_json['bowler']]:
-                match_info[pub_json['bowler']]['wickets'] += 1
-            else:
-                match_info[pub_json['bowler']]['wickets'] = 1
         #   -------------------------------------------------------------------------------------------------------    #
 
         return match_info
@@ -467,9 +413,7 @@ def lambda_handler(event, context):
 
             insert_into_es(match_info, es_config['es_matches_index'], int(match_info['match_id']))
 
-            # print(player_info_1)
-            # print(player_info_2)
-            insert_into_es(player_info_1['_source'], es_config['es_players_index'], pid_1)
-            insert_into_es(player_info_2['_source'], es_config['es_players_index'], pid_2)
+            insert_into_es(player_info_1, es_config['es_players_index'], pid_1)
+            insert_into_es(player_info_2, es_config['es_players_index'], pid_2)
 
     # response = requests.post('http://34.227.172.158:3718/', json=json.dumps(currentEvent))
